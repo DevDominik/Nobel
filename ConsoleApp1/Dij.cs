edb0152778b84ec0e118f3a0b44c85e9fd21a8e1
DevDominik <kecskes.dominik.hun@gmail.com>
2023-06-01 10:56:08 +0200

    kész

---
+++ ConsoleApp1/Dij.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Dij
+    {
+        int evPriv;
+        string tipusPriv, keresztnevPriv, vezeteknevPriv;
+        public Dij(string bevitel)
+        {
+            string[] bontott = bevitel.Split(';');
+            this.evPriv = int.Parse(bontott[0]);
+            this.tipusPriv = bontott[1];
+            this.keresztnevPriv = bontott[2];
+            this.vezeteknevPriv = bontott[3];
+        }
+
+        public int Ev { get => evPriv; }
+        public string Tipus { get => tipusPriv; }
+        public string Keresztnev { get => keresztnevPriv; }
+        public string Vezeteknev { get => vezeteknevPriv; }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ConsoleApp1
 {
@@ -10,7 +11,44 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            //aaa
+            List<Dij> dijak = File.ReadAllLines("nobel.csv").Skip(1).Select(x => new Dij(x)).ToList();
+            Console.WriteLine("4. feladat: 2017-ben ezek az emberek kaptak irodalmi Nobel-díjat:");
+            foreach (Dij dij in dijak)
+            {
+                if (dij.Ev == 2017 && dij.Tipus == "irodalmi")
+                {
+                    Console.WriteLine($"{dij.Keresztnev} {dij.Vezeteknev}");
+                }
+            }
+            Console.WriteLine("6. feladat: A Curie-család tagjai ekkor kaptak Nobel-díjat:");
+            foreach (Dij dij in dijak)
+            {
+                if (dij.Vezeteknev.Contains("Curie"))
+                {
+                    Console.WriteLine($"{dij.Keresztnev} {dij.Vezeteknev} | {dij.Ev}");
+                }
+            }
+            Console.WriteLine("7. feladat: A Nobel-díjak típus szerint összegezve:");
+            List<string> tipusok = new List<string>();
+            foreach (Dij dij in dijak)
+            {
+                if (!tipusok.Contains(dij.Tipus))
+                {
+                    tipusok.Add(dij.Tipus);
+                }
+            }
+            foreach (string item in tipusok)
+            {
+                Console.WriteLine($"\t{item}: {dijak.Count(x => x.Tipus == item)} db");
+            }
+            dijak.OrderBy(x => x.Ev);
+            StreamWriter sw = new StreamWriter("orvosi.txt", false);
+            foreach (Dij dij in dijak)
+            {
+                sw.WriteLine($"{dij.Ev};{dij.Keresztnev} {dij.Vezeteknev}");
+            }
+            sw.Close();
+            Console.WriteLine("8. feladat: orvos.txt");
         }
     }
 }
